e67092cdc832714ed887484943f4c75e6e0ff6a4
Md Imran Hossen <freelancerimran81@gmail.com>
2022-09-16 19:01:47 +0600

    navbar solved

--- src/Components/Home/Header/Navbar.js
+++ src/Components/Home/Header/Navbar.js
@@ -36,8 +36,8 @@ const Navbar = () => {
                         </ul>
                     </li> */}
                     <li><CustomLink to='/crossfit' className=" hover_effect font-bold text-sm text-[12px] p-2" href="#">CrossFit</CustomLink></li>
-                    <li><CustomLink to='/programs' className="uppercase hover_effect font-bold text-sm text-[12px] p-2" href="#">Programs</CustomLink></li>
-                    <li><CustomLink to='/trainers' className="uppercase hover_effect font-bold text-sm text-[12px] p-2" href="#">Trainers</CustomLink></li>
+                    <li><CustomLink to='/programs' className="uppercase hover_effect font-bold text-sm text-[12px] w-[130px] p-2" href="#">Other Programs</CustomLink></li>
+                    <li><CustomLink to='/trainers' className="uppercase hover_effect font-bold text-sm text-[12px] w-20 p-2" href="#">Our Team</CustomLink></li>
                     <li><CustomLink to='/testimonials' className="uppercase hover_effect font-bold text-sm text-[12px] p-2" href="#">Testimonials</CustomLink></li>
 
                     <li><CustomLink to='/blog' className="uppercase hover_effect font-semibold text-sm text-[12px] p-2" href="#">Blog</CustomLink></li>
@@ -60,7 +60,7 @@ const Navbar = () => {
                             <li><Link to='/' className="uppercase hover_effect font-bold text-sm" href="#">Home</Link></li>
                             <li><Link to='/about' className="uppercase hover_effect font-bold text-sm" href="#">About</Link></li>
                             <li><Link to='/crossFit' className=" hover_effect font-bold text-sm" href="#">CrossFit</Link></li>
-                            <li><Link to='/programs' className="uppercase hover_effect font-bold text-sm" href="#">Other Program</Link></li>
+                            <li><Link to='/programs' className="uppercase hover_effect font-bold text-sm" href="#">Other Programs</Link></li>
                             <li><Link to='/trainers' className="uppercase hover_effect font-bold text-sm" href="#">Our Team</Link></li>
                             <li><Link to='/testimonials' className="uppercase hover_effect font-bold text-sm" href="#">Testimonials</Link></li>
                             <li><Link to='/blog' className="uppercase hover_effect font-bold text-sm" href="#">Blog</Link></li>
