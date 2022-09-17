44ea12962b3147cd721f45ed080dc31526f9d8b4
Ayon Jodder <ayonjodder177@gmail.com>
2022-09-18 05:35:52 +0600

    All done

--- src/Dashboard/TrainerDashboard/StudentList/StudentList.js
+++ src/Dashboard/TrainerDashboard/StudentList/StudentList.js
@@ -86,10 +86,10 @@ const StudentList = () => {
                                         <div className="modal">
                                             <div className="modal-box relative">
                                                 <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                                                <h3 className="text-lg font-bold">Information of {studentData?.name}</h3>
+                                                <h3 className="text-lg font-bold">Information of {studentData?.name ? studentData?.name : 'User'}</h3>
                                                 <p>
                                                     <strong>Name: </strong>
-                                                    {studentData?.name}
+                                                    {studentData?.name ? studentData?.name : 'User'}
                                                 </p>
                                                 <p>
                                                     <strong>Email: </strong>
